6b40ae744ad4e8bc42344c790c645f972124a081
Seyed Hasan Mousavi <mousavi.sc11@gmail.com>
2019-05-20 02:12:01 +0430

    Isna News parser has been implemented.

---
+++ core/parser.py
@@ -0,0 +1,97 @@
+import re
+
+
+def isna_parser(content: str) -> list:
+    # extract information and return data for saving in the News model
+    # now just parse <p>, <strong>, <img>
+    # todo: parse video
+    # todo: parse other tags
+    # return jsonify data
+
+    array = []
+    temp_var = ''
+    open_tag = False
+    # remove outer tag
+    content = content[46:-6]
+
+    # put tags and texts in arrays
+    for char in content:
+        if char == '<':
+            open_tag = True
+            if len(temp_var) > 0:
+                array.append(temp_var)
+                temp_var = ''
+            temp_var += char
+        elif char == '>':
+            open_tag = False
+            temp_var += char
+            array.append(temp_var)
+            temp_var = ''
+        elif char == ' ':
+            if len(temp_var) > 0 and not open_tag:
+                array.append(temp_var)
+                temp_var = ''
+            else:
+                temp_var += char
+        else:
+            temp_var += char
+
+    # print(array)
+
+    # parse array of tags and texts
+
+    content = []
+    current_tag = ''
+    temp_array = []
+    temp_dict = {}
+
+    for item in array:
+
+        if item[0:2] == '<p':
+            # make a dict
+            if len(temp_dict.keys()) == 0:
+                temp_dict['p'] = []
+                current_tag = 'p'
+
+        elif item[0:3] == '</p':
+            # close current dict and add it to array
+            if len(temp_array) > 0:
+                temp_dict[current_tag].append({'normal': ' '.join(temp_array)})
+                content.append(temp_dict)
+                temp_array = []
+                temp_dict = {}
+
+        elif item[0:7] == '<strong':
+            if len(temp_array) > 0:
+                temp_dict[current_tag].append({'normal': ' '.join(temp_array)})
+                temp_array = []
+        elif item[0:8] == '</strong':
+            # add data
+            temp_dict[current_tag].append({'bold': ' '.join(temp_array)})
+            temp_array = []
+        elif item[0:2] == '<a':
+            # make a dict
+            pass
+        elif item[0:3] == '</a':
+            # close current dict
+            pass
+        elif item[0:4] == '<img':
+            if len(temp_array) > 0:  # todo: should save current tag state
+                temp_dict[current_tag].append({'normal': ' '.join(temp_array)})
+                temp_array = []
+            matches = re.search('src="([^"]+)"', item)
+            temp_dict[current_tag].append({'image': matches[0][4:]})
+        elif item[0:4] == '<div':
+            # video_link todo: handle video
+            pass
+        elif item[0:5] == '</div':
+            pass
+        else:
+            # other tags and texts
+            if item[0] == '<':
+                pass
+            else:
+                temp_array.append(item)
+
+    print(content)
+    return content
--- core/tasks.py
+++ core/tasks.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 from celery import task
@@ -12,11 +13,10 @@ from core.parser import isna_parser
 
 @task()
 def news_crawler():
-    print('task is executed!')
+    print(000000000000)
     sources = NewsSource.objects.filter(active=True)
     for source in sources:
         resp = requests.get(source.url)
-        # print(resp)
         soup_xml = BeautifulSoup(resp.content, features="xml")
         items = soup_xml.findAll('item')
         source.last_crawl_date = now()
@@ -24,27 +24,6 @@ def news_crawler():
         for item in items:
             news_link = item.link.text
             url_handler.delay(news_link, source.id)
-            # resp = requests.get(news_link)
-            # soup_lxml = BeautifulSoup(resp.content, features="lxml")
-            # title = soup_lxml.find('h1', class_='first-title')
-            # # news_summery = soup_lxml.find('p', class_='summary')
-            # news_text = soup_lxml.find('div', itemprop='articleBody')
-            # news_id = soup_lxml.select_one('#item > div.news-info > div > ul > li:nth-child(3) > span.text-meta')
-            # published_date = soup_lxml.find('meta', itemprop='datePublished')['content']
-
-
-            # try:
-            #     News.objects.create(
-            #         url=news_link,
-            #         title=title.text,
-            #         source=source,
-            #         news_id=news_id.text if news_id else None,
-            #         content=news_text.text,
-            #         published_at=parse_datetime(published_date)
-            #     )
-            # except Exception as e:
-            #     print(e)
-
 
 
 
@@ -59,8 +38,16 @@ def url_handler(url: str, source_id: int):
     resp = requests.get(url)
     soup_lxml = BeautifulSoup(resp.content, features="lxml")
     title = soup_lxml.find('h1', class_='first-title')
-    # news_summery = soup_lxml.find('p', class_='summary')
-    news_text = soup_lxml.find('div', itemprop='articleBody')
+    news_summery = soup_lxml.find('p', class_='summary').text
+    main_image = soup_lxml.select_one('#item > div.full-news > div.full-news-text > div.item-body.content-full-news > figure > img')
+    header_data = []
+    if main_image:
+        main_image_source = main_image['src']
+        header_data.append({'image': str(main_image_source)})
+    header_data.append({'normal': str(news_summery)})
+    news_parsed_data = isna_parser(str(soup_lxml.find('div', itemprop='articleBody')))
+    news_data = [{'p': header_data}] + news_parsed_data
+    news_json_data = json.dumps(news_data)
     news_id = soup_lxml.select_one('#item > div.news-info > div > ul > li:nth-child(3) > span.text-meta')
     published_date = soup_lxml.find('meta', itemprop='datePublished')['content']
 
@@ -70,7 +57,7 @@ def url_handler(url: str, source_id: int):
             title=title.text,
             source=NewsSource.objects.get(id=source_id),
             news_id=news_id.text if news_id else None,
-            content=news_text.text,
+            content=news_json_data,
             published_at=parse_datetime(published_date)
         )
     except Exception as e:
